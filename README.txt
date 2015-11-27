fdfeccf987dd2c9813eebc83688fe5f3d686f1b5
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-11-27 10:05:09 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,32 @@
+# MySQL tmpfs Docker image
+
+## How to use
+
+```
+docker run --name tmp-mysql --privileged -d mysql-tmpfs
+```
+
+## Environment Variables
+
+### `MYSQL_TMPFS_SIZE`
+
+The size of the tmpfs partition, in MB (default: 256)
+
+### `MYSQL_INIT_FILE`
+
+Location of the MySQL init-file. Can be mounted via the -v option:
+
+```
+mkdir mysql-init
+cat > ./mysql-init/init.sql <<-EOSQL
+DELETE FROM mysql.user ;
+CREATE USER 'root'@'%' IDENTIFIED BY 'mysecret' ;
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+
+CREATE DATABASE mydatabase ;
+...
+EOSQL
+docker run --name tmp-mysql --privileged -p 3306:3306 -v $(pwd)/mysql-init:/mysql-init -e MYSQL_INIT_FILE=/mysql-init/init.sql -d mysql-tmpfs
+```
+
+If no MYSQL_INIT_FILE is given, a default one (root user, no password) is created.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+DATADIR='/var/lib/mysql'
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- mysqld "$@"
+fi
+
+MYSQL_TMPFS_SIZE=${MYSQL_TMPFS_SIZE:-"256"}
+
+if [ ! -d "$DATADIR/mysql" ]; then
+	echo "Creating tmpfs with ${MYSQL_TMPFS_SIZE} MB ..."
+	mkdir -p "$DATADIR"
+	mount -t tmpfs -o size="${MYSQL_TMPFS_SIZE}m" tmpfs "$DATADIR"
+fi
+
+if [ "${1%_safe}" = 'mysqld' ]; then
+	echo 'Running mysql_install_db ...'
+	mysql_install_db
+	echo 'Finished mysql_install_db'
+
+	if [ -e "$MYSQL_INIT_FILE" ]; then
+		echo 'Setting init file ...'
+
+		# Allow user to provide their own init file
+		# This has to include all privileges, users and databases,
+		# no further SQL is being executed if this file is present
+
+		set -- "$@" --init-file="$MYSQL_INIT_FILE"
+	else
+		echo 'Creating default init file ...'
+		tempSqlFile='/tmp/mysql-init.sql'
+		cat > "$tempSqlFile" <<-EOSQL
+			DELETE FROM mysql.user ;
+			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+		EOSQL
+
+		if [ "$MYSQL_DATABASE" ]; then
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
+		fi
+		
+		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
+			
+			if [ "$MYSQL_DATABASE" ]; then
+				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
+			fi
+		fi
+
+		echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
+
+		set -- "$@" --init-file="$tempSqlFile"
+	fi
+fi
+
+chown -R mysql:mysql "$DATADIR"
+exec "$@"
